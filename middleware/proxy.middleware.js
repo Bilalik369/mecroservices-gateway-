cdfd492af5c15144b8f5db0f88996fa75df07d34
Bilalik369 <bilal11iken@gmail.com>
2025-07-16 12:55:55 +0100

    ajoute de la fonction createProxyservice pour gerer le proxy vers les mecroservices

---
+++ middleware/proxy.middleware.js
@@ -0,0 +1,55 @@
+import { error } from "console";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+
+
+
+const services = {
+    auth: {
+        target: process.env.AUTH_SERVICE_URL ,
+        pathRewrite: { '^/api/auth': '/api/auth' }
+      },
+      apprenants: {
+        target: process.env.APPRENANT_SERVICE_URL ,
+        pathRewrite: { '^/api/apprenants': '/api' }
+      },
+      briefs: {
+        target: process.env.BRIEF_SERVICE_URL,
+        pathRewrite: { '^/api/briefs': '/api' }
+      },
+      competences: {
+        target: process.env.COMPETENCE_SERVICE_URL,
+        pathRewrite: { '^/api/competences': '/api' }
+      }
+    };
+
+
+    
+    export const createServiceProxy = (serviceName) => {
+        const serviceConfig = services[serviceName];
+        
+        if (!serviceConfig) {
+          throw new Error(`Service ${serviceName} non configuré`);
+        }
+      
+        return createProxyMiddleware({
+          target: serviceConfig.target,
+          changeOrigin: true,
+          pathRewrite: serviceConfig.pathRewrite,
+          onError: (err, req, res) => {
+            console.error(`Erreur proxy vers ${serviceName}:`, err.message);
+            res.status(503).json({
+              success: false,
+              message: `Service ${serviceName} indisponible`
+            });
+          },
+          onProxyReq: (proxyReq, req, res) => {
+           
+            if (req.user) {
+              proxyReq.setHeader('X-User-Id', req.user.id);
+              proxyReq.setHeader('X-User-Role', req.user.role);
+              proxyReq.setHeader('X-User-Email', req.user.email);
+            }
+          }
+        });
+      };
