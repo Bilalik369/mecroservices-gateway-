db4232aea8c6c1ec09046dade7df54e8c154ba98
Bilalik369 <bilal11iken@gmail.com>
2025-07-16 21:37:18 +0100

    securisation des route avec middleware d'authentification et prxy vers mecroservices

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -55,3 +55,4 @@ export const checkRole  = (allowedRoles)=>{
 
     }
 }
+
--- routes/gateway.routes.js
+++ routes/gateway.routes.js
@@ -1,11 +1,30 @@
-import express from "express ";
+import express from 'express';
+import { verifyToken, checkRole } from '../middleware/auth.middleware.js';
+import { createServiceProxy } from '../middleware/proxy.middleware.js';
+
+const router = express.Router();
 
 
+router.use('/api/auth', createServiceProxy('auth'));
 
 
-const router = express.Router()
+router.use('/api/apprenants', 
+  verifyToken,
+  checkRole(['ADMIN', 'FORMATEUR', 'APPRENANT']),
+  createServiceProxy('apprenants')
+);
+
+router.use('/api/briefs', 
+  verifyToken,
+  checkRole(['ADMIN', 'FORMATEUR', 'APPRENANT']),
+  createServiceProxy('briefs')
+);
 
 
+router.use('/api/competences', 
+  verifyToken,
+  checkRole(['ADMIN', 'FORMATEUR']),
+  createServiceProxy('competences')
+);
 
-
-export default router
+export default router;
