9ab20821495f054390a38a71c69f6cbe8023383f
Bilalik369 <bilal11iken@gmail.com>
2025-07-16 11:43:19 +0100

    la connexion avec base des donnees

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js"
 
 
 
@@ -14,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT 
 
 
-
+connectDB();
 app.listen(PORT , ()=>{
     console.log(`server is ranning in PORT ${PORT}`)
 })
